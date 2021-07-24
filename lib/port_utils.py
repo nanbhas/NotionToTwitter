a7b135f33c774fa8f969591dc059feb934aa5b53
Nandita Bhaskhar <nanbhas@stanford.edu>
2021-07-24 11:11:45 -0700

    add port fns - sort by order, filter by postdate, starter

--- lib/port_utils.py
+++ lib/port_utils.py
@@ -22,7 +22,6 @@ def getAllUntweetedRowsFromNotionDatabase(notion, notionDB_id):
         notionDB_id: (str) string code id for the relevant database
     Returns:
         allNotionRows: (list of notion rows)
-
     '''
     start = time.time()
     hasMore = True
@@ -78,5 +77,46 @@ def getAllUntweetedRowsFromNotionDatabase(notion, notionDB_id):
     print('Total time taken: ' + str(end-start))
 
     return allNotionRows
+
+
+def filterRowsToBePostedBasedOnDate(allRows, datetime):
+    '''
+    Filters rows (notion pages) from a list of rows whose 'Post Date' matches the given datetime
+    Args:
+        allRows: (list of notion rows)  each row should contain a date property named Post Date  
+        datetime: (arrow/str/datetime) representation of datetime
+    Returns:
+        filteredRows: (list of notion rows)    
+    '''
+    arrowTime = arrow.get(datetime).to('US/Pacific')
+    filteredRows = [item for item in allRows if arrow.get(item['properties']['Post Date']['date']['start']).date() == arrowTime.date()]
+    
+    return filteredRows
+
+
+def getStarterRows(allRows):
+    '''
+    Filters rows (notion pages) from a list of rows whose 'Children Tweets' column is not empty
+    Args:
+        allRows: (list of notion rows)  each row should contain a relation property named Children Tweets  
+    Returns:
+        starterRows: (list of notion rows)    
+    '''
+    starterRows = [item for item in allRows if len(item['properties']['Children Tweets']['relation']) > 0 ]
+    
+    return starterRows
+
+
+def sortRowsByOrder(subsetRows):
+    '''
+    Sort rows (notion pages) from a list of rows based on their 'Order' property
+    Args:
+        subsetRows: (list of notion rows)  each row should contain a property named Order
+    Returns:
+        sortedRows: (list of notion rows) sorted by their order property   
+    '''
+    sortedRows = sorted(subsetRows, key = lambda i: i['properties']['Order']['number'])
+    
+    return sortedRows
 
 
