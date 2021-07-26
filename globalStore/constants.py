c1a1c3391e9f4afe7edc8c43714a3f62f288dd33
Nandita Bhaskhar <nanbhas@stanford.edu>
2021-07-25 20:37:12 -0700

    make some constants private

--- globalStore/constants.py
+++ globalStore/constants.py
@@ -2,8 +2,12 @@
 Author: Nandita Bhaskhar
 All global constants
 """
+import sys
+sys.path.append('../')
+
+import globalStore.privateConstants as privateConstants
 
 ### All global constants here
 
-TWITTER_SECRET_FILE = "../secrets/secrets_twitter.json"
+TWITTER_SECRET_FILE = privateConstants.TWITTER_SECRET_FILE_TEST
 NOTION_SECRET_FILE = "../secrets/secrets_notion.json"
